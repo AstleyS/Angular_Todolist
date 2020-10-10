1ca01252182db2ba8f9d848d14a25ec4a0e2afdd
Astley Santos <astley_394@hotmail.com>
2020-10-10 21:20:36 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+// Meta data
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  // declare type of variable with variable:type
+  name:string = 'Astley';
+
+  //cosntructor used to import services
+
+}
